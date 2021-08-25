f57475e5bb1dd1dcae354b43c6892a3d4784f4ac
fatih <fatihenaz@gmail.com>
2021-08-25 13:52:32 +0300

    api added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import './App.css';
 import Button from './components/Button';
 import Card from "./components/Card"
-import axios from "axios"
+import axios from "axios";
 import { useState,useEffect } from 'react';
 
-function App() {
-  const [itemList, setItemList] = useState([]);
+const App = () => {
+  const [image, setImage] = useState("");
+  const [email, setEmail] = useState("");
+  const [cell, setCell] = useState("");
+  const [name, setName] = useState("");
+  const [country, setCountry] = useState("");
+  const [age, setAge] = useState("");
+  
 
-  useEffect(){
+
+
+
+  useEffect(() => {
+    axios
+      .get("https://randomuser.me/api/").then(res => setImage(res.data.results[0].picture.large))
+    axios  
+    .get("https://randomuser.me/api/").then(res => setEmail(res.data.results[0].email))
+    axios  
+    .get("https://randomuser.me/api/").then(res => setCell(res.data.results[0].cell))
+    axios  
+    .get("https://randomuser.me/api/").then(res => setName(res.data.results[0].name.first))
+    axios  
+    .get("https://randomuser.me/api/").then(res => setCountry(res.data.results[0].location.country))
+ 
+    axios  
+    .get("https://randomuser.me/api/").then(res => setAge(res.data.results[0].dob.age))
+})
+
     
-}
-
-
-
-
-
 
   return (
     <div className="App">
-      <Card />
+      <Card image={image} email={email} cell={cell} name={name} country={country} age={age}/>
       <Button />
 
     </div>
   );
-}
+
+  }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,30 +4,30 @@ import email from "../assets/email.svg"
 import location from "../assets/location.svg"
 import phone from "../assets/phone.svg"
 
-const Card = ({item}) => {
+const Card = (props) => {
     return (
         
         <div className="container mt-5">
             <div className="info-box"> 
                 <div className="box">
-                    <img className="avatar" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH9Qo-qgAh4JYCzZnDYpHNlIqnEm7N526slHUL6dKzoxvTsGDoEL1pxrM-E_BIHANDUpQ&usqp=CAU"></img>    
-                    <span><h3>Polat Çakır</h3></span>
+                    <img className="avatar" src={props.image}></img>    
+                    <span><h3>{props.name }</h3></span>
                 </div>
                 <div className="box">
                     <img height={40} width={40} src={email} alt="email"></img>   
-                    <span><p>email@email.com</p></span>
+                    <span><p>{props.email }</p></span>
                 </div>
                 <div className="box">
                     <img height={40} width={40} src={phone}></img>
-                    <span><p>0507 489 66 33</p></span>  
+                    <span><p>{props.cell }</p></span>
                 </div>
                 <div className="box">
                     <img height={40} width={40} src={location}></img>
-                    <span><p>Çerkezköy - Tekirdağ</p></span>
+                    <span><p>{ props.country}</p></span>
                 </div>  
             </div>
             <div className="footer">
-                <p>Age:40</p>
+                <p>{props.age}</p>
                 <p>Register Date:12-12-2037</p>
             </div>       
         </div>
