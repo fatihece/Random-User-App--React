e7e722a7f999466add91f85fcbe839cc96db8459
fatih <fatihenaz@gmail.com>
2021-10-24 14:55:47 +0300

    card component updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,35 +1,53 @@
-import React from 'react'
 import "./Card.css"
 import email from "../assets/email.svg"
 import location from "../assets/location.svg"
 import phone from "../assets/phone.svg"
+import axios from "axios";
+import { useState, useEffect } from 'react';
+import Button from './Button';
 
-const Card = (props) => {
+const Card = () => {
+    const [item, setItem] = useState("");
+    const [refresh, setRefresh] = useState(true);
+  
+    const onButtonClick = () => {
+        setRefresh(!refresh)
+      }
+  
+    
+    useEffect(() => {
+        axios.get("https://randomuser.me/api/")
+        .then((res) => setItem(res.data.results[0]))
+    }, [refresh])
+    console.log(item)
+
+
     return (
         
         <div className="container mt-5">
             <div className="info-box"> 
                 <div className="box">
-                    <img className="avatar" src={props.image} />   
-                    <h3>{props.title} {props.name} { props.last}</h3>
+                    <img className="avatar" src={item?.picture?.large} alt=""/>   
+                    <h3>{item?.name?.title} {item?.name?.first} { item?.name?.last}</h3>
                 </div>
                 <div className="box">
                     <img height={40} width={40} src={email} alt="email"/>
-                    <p>{props.email}</p>
+                    <p>{item?.email}</p>
                 </div>
                 <div className="box">
                     <img height={40} width={40} src={phone} alt="phone"/>
-                    <p>{props.cell}</p>
+                    <p>{item?.cell}</p>
                 </div>
                 <div className="box">
                     <img height={40} width={40} src={location} alt="location"/>
-                    <p>{props.city} / {props.country}</p>
+                    <p>{item?.location?.city} / {item?.location?.country}</p>
                 </div>  
             </div>
             <div className="footer">
-                <p>{`Age: ${props.age}`}</p>
-                <p>{`Register Date: ${props.date}`}</p>
-            </div>       
+                <p>{`Age: ${item?.dob?.age}`}</p>
+                <p>{`Register Date: ${item?.registered?.date.slice(0,10)}`}</p>
+            </div>
+            <Button onButtonClick={onButtonClick}/>
         </div>
     )
 }
